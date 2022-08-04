a35fe1a23fe2ee5ed3c97463a7f8ca63c72dbb91
a <a@example.com>
2022-08-05 01:46:34 +0900

    add random test

--- test2/consistency.ts
+++ test2/consistency.ts
@@ -4,6 +4,7 @@ import { createFixture } from "./fixtures";
 import { checkConsistency, getKeys } from "./helper";
 import { expect } from "chai";
 import _ from "lodash";
+import MersenneTwister from "mersenne-twister";
 
 describe("consistency", () => {
   let loadFixture = waffle.createFixtureLoader(waffle.provider.getWallets());
@@ -39,6 +40,38 @@ describe("consistency", () => {
       }
       expect(await getKeys(tree)).to.deep.eq(_.sortBy([2, 8, 5, 7, 6]));
       await checkConsistency(tree);
+    });
+  });
+
+  describe("random", () => {
+    it("ok", async () => {
+      const generator = new MersenneTwister(1);
+
+      let keys = [];
+      for (let i = 0; i < 100; i++) {
+        const op = generator.random_int() % 100;
+        if (op < 40) {
+          const key = 1 + generator.random_int();
+          if (_.includes(keys, key)) continue;
+          keys.push(key);
+          await tree.insert(key);
+        } else if (op < 80) {
+          if (_.isEmpty(keys)) continue;
+          const idx = generator.random_int() % keys.length;
+          const key = keys[idx];
+          keys = _.filter(keys, (x) => x != key);
+          await tree.remove(key);
+        } else {
+          if (_.isEmpty(keys)) continue;
+          const idx = generator.random_int() % keys.length;
+          const key = keys[idx];
+          keys = _.filter(keys, (x) => x > key);
+          await tree.removeLeft(key);
+        }
+
+        expect(await getKeys(tree)).to.deep.eq(_.sortBy(keys));
+        await checkConsistency(tree);
+      }
     });
   });
 });
